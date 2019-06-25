1c675ac5ef99024fa0f16489a49fa0d92b820796
kamgark <kamgark@gmail.com>
2019-06-25 16:14:35 +0500

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <div>
+            <footer className="page-footer transparent">
+                <div className="container">
+                </div>
+                <div className="footer-copyright grey darken-3">
+                    <div className="container center-align">
+                        &copy; 2019 WYKA Recruitment System <br />
+                        Created By WYKA Solution DEVS.
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
